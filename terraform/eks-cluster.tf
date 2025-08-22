8a7c5e9fccab4d680731331d5cb07fbeac710b4a
Anes Ben Henda <anes.bhd12@gmail.com>
2025-08-23 00:37:44 +0100

    Ensure VPC is created first

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -5,36 +5,35 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
-
+      name           = "node-group-1"
       instance_types = ["t3.small"]
 
       min_size     = 1
       max_size     = 3
       desired_size = 2
-      depends_on   = [module.eks.aws_eks_cluster.this]
     }
 
     two = {
-      name = "node-group-2"
-
+      name           = "node-group-2"
       instance_types = ["t3.small"]
 
       min_size     = 1
       max_size     = 2
       desired_size = 1
-      depends_on   = [module.eks.aws_eks_cluster.this]
     }
   }
+
+  # Ensure VPC is created first, and force waiting until the cluster is ready
+  depends_on = [module.vpc]
 }
