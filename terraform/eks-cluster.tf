9f25290b8b7e65ded9f31cb67efca9535b16f18c
Anes Ben Henda <anes.bhd12@gmail.com>
2025-08-23 20:00:42 +0100

    fixing timing issues with EKS cluster creation

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,37 +1,42 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.19.1"
+  version = "20.8.5"
 
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
 
-  vpc_id                        = module.vpc.vpc_id
-  subnet_ids                    = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
   cluster_endpoint_public_access = true
+
+  create_cloudwatch_log_group = false
+  create_kms_key              = false
+  cluster_encryption_config   = []
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
   }
 
-  create_cloudwatch_log_group = false
-  create_kms_key              = false
-  cluster_encryption_config    = []
-
   eks_managed_node_groups = {
     one = {
       name           = "node-group-1"
       instance_types = ["t3.small"]
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
     }
 
     two = {
       name           = "node-group-2"
       instance_types = ["t3.small"]
-      min_size       = 1
-      max_size       = 2
-      desired_size   = 1
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
     }
   }
+
+  depends_on = [module.vpc]
 }
